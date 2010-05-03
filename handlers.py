55d7b995c7e7d3b958751a2921d947df89d3e7b5
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2010-05-03 15:36:00 +0200

    Added update and delete methods to RecordHandler

--- handlers.py
+++ handlers.py
@@ -2,10 +2,31 @@ from piston.handler import BaseHandler
 from sumatra.recordstore.django_store import models
 import datetime
 
+"""
+Note that in the Wikipedia REST article, the verb table differs slightly from
+what is in the Piston documentation. In Wikipedia, it states that for an
+element URI (e.g. a simulation record here), PUT should be used to create a
+new element with a specified ID, while POST should be used to treat the resource
+as a collection and add a subordinate. Piston says use POST to an element URI
+to create a new resource. The Wikipedia approach seems more logical to me, so I'm
+using it.
+"""
+
 one_second = datetime.timedelta(0, 1)
 
+def build_filter(**kwargs):
+    D = {}
+    for key in 'year', 'month', 'day', 'hour', 'minute', 'second':
+        D[key] = int(kwargs[key])
+    timestamp = datetime.datetime(**D)
+    filter = {'timestamp__gte': timestamp,
+              'timestamp__lt': timestamp + one_second}
+    filter['project'] = kwargs['project']
+    filter['group'] = kwargs['group']
+    return filter
+
 class RecordHandler(BaseHandler):
-    allowed_methods = ('GET', 'PUT', 'POST', 'DELETE')
+    allowed_methods = ('GET', 'PUT', 'DELETE')
     model = models.SimulationRecord
     fields = ('project', 'group', 'timestamp', 'reason', 'outcome', 'duration',
               'executable', 'repository', 'main_file', 'version', 'diff',
@@ -13,14 +34,32 @@ class RecordHandler(BaseHandler):
               'data_key', 'platforms', 'tags')
     
     def read(self, request, project, group, year, month, day, hour, minute, second):
-        D = {}
-        for key in 'year', 'month', 'day', 'hour', 'minute', 'second':
-            D[key] = int(locals()[key])
-        timestamp = datetime.datetime(**D)
-        return BaseHandler.read(self, request,
-                                project=project, group=group,
-                                timestamp__gte=timestamp,
-                                timestamp__lt=timestamp + one_second)
+        filter = build_filter(**locals())
+        return self.queryset(request).get(**filter)
+    
+    def update(self, request, project, group, year, month, day, hour, minute, second):
+        # this performs update if the record already exists, and create otherwise
+        filter = build_filter(locals())
+        attrs = self.flatten_dict(request.data)
+        try:
+            # need to check consistency between URL project, group, timestamp
+            # and the same information in request.data
+            # we should also limit the fields that can be updated
+            inst = self.queryset(request).get(**filter)
+            for k,v in attrs.iteritems(): # not sure this handles foreign key fields
+                setattr(inst, k, v)
+            inst.save()
+            return rc.ALL_OK
+        except self.model.DoesNotExist:
+            inst = self.model(**attrs) # not sure this handles foreign key fields
+            inst.save()
+            return rc.CREATED
+        except self.model.MultipleObjectsReturned: # this should never happen
+            return rc.DUPLICATE_ENTRY
+
+    def delete(self, request, project, group, year, month, day, hour, minute, second):
+        filter = build_filter(locals())
+        return BaseHandler.delete(self, request, **filter)
     
 
 # the following are currently defined only to suppress the 'id'
